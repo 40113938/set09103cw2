3e3d5db2fdbe4a57610434a283ad373ee4ea3ad4
40113938 <richard_cook@hotmail.co.uk>
2015-11-24 17:47:11 +0000

    More things

--- cw2/app/app.py
+++ cw2/app/app.py
@@ -1,4 +1,9 @@
-import datetime, functools, os, re, urllib
+#app.py
+import datetime
+import functools
+import os
+import re
+import urllib
 
 from flask import (Flask, abort, flash, Markup, redirect, render_template,
 request, Response, session, url_for)
@@ -6,14 +11,14 @@ from markdown import markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.extra import ExtraExtension
 from micawber import bootstrap_basic, parse_html
-from micwber.cache import Cache as OEmbedCache
+from micawber.cache import Cache as OEmbedCache
 from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
-ADMIN_PASS = 'password'
+ADMIN_PASSWORD = 'password'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-DATABASE = 'sqliteext:///%s' ps.path.join(APP_DIR, 'blog.db')
+DATABASE = 'sqliteext:///%s' %  os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
 SECRET_KEY = 'secret'
 SITE_WIDTH = 800
@@ -21,17 +26,18 @@ SITE_WIDTH = 800
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 flask_db = FlaskDB(app)
 database = flask_db.database
 
-oembed_providers = bootstrap_basic(0EmbedCache())
+oembed_providers = bootstrap_basic(OEmbedCache())
 
 class Entry(flask_db.Model):
   title = CharField()
   slug = CharField(unique=True)
   content=TextField()
   published = BooleanField(index=True)
-  timestamp = DateTimeField(default=datetime.datetime.now. index=True)
+  timestamp = DateTimeField(default=datetime.datetime.now, index=True)
 
   def save(self, *args, **kwargs):
     if not self.slug:
@@ -58,7 +64,49 @@ class FTSEntry(FTSModel):
   content = TextField()
 
   class Meta:
-    database = database
+    database=database
 
+@app.template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+
+  querystring= dict((key, value) for key, value in request_args.items())
+  for key in keys_to_remove:
+    querystring.pop(key, None)
+  querystring.update(new_values)
+  return urllib.urlencode(querystring)
+
+@app.errorhandler(404)
+def not_found(exc):
+  return Repsonse('<h3>Not found</h3>'), 404
+
+def login_required(fn):
+  @functools.wraps(fn)
+  def inner(*args, **kwargs):
+    if session.get('logged_in'):
+      return fn(*args, **kwargs)
+    return redirect(url_for('login', next=request.path))
+  return inner
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+  next_url = request.args.get('next') or request.form.get('next')
+  if request.method == 'POST' and request.form.get('password'):
+    password = request.form.get('password')
+    if password == app.config['ADMIN_PASSWORD']:
+      session['logged in'] = True
+      session.permanent = True
+
+      flash('you are now logged in','success')
+      return redirect(next_url or url_for('index'))
+    else:
+      flash('incorrect pass', 'danger')
+  return render_template('login.html', next_url=next_url)
+
+def main():
+  database.create_tables([Entry, FTSEntry], safe=True)
+  app.run(debug=False)
+
+if __name__ == '__main__':
+  main()
 
 
