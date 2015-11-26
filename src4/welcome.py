4c198a954286198df5f1b517a15a1a36522f516f
40113938 <richard_cook@hotmail.co.uk>
2015-11-26 14:35:34 +0000

    Fixed Werkzeug errors

--- src4/welcome.py
+++ src4/welcome.py
@@ -7,20 +7,10 @@ def get_db():
 
 @app.route("/welcome/", methods=['POST', 'GET'])
 def welcome():
-  if request.method == 'POST':
-    print request.form
-    print "its all over richard"
-    search = request.form['search']
-    return "You searched for %s" % search
-  else:
     return render_template('welcome.html')
 
-@app.route("/login/", methods=['POST','GET'])
-def login():
-  return render_template('login.html')
-
-@app.route("/artist/", methods=['POST', 'GET'])
-def artist():
+@app.route("/films/", methods=['POST', 'GET'])
+def films():
   band = request.args.get('q', '')
   g.db = get_db()
   if band == '':
@@ -29,10 +19,10 @@ def artist():
     cur = g.db.execute ('SELECT * from film where name = ?', (band, ))
   film = [dict(name=row[0],
   yoc=row[1], main_actor=row[2], genre=row[3]) for row in cur.fetchall()]
-  return render_template('artist.html', band=band, film=film)
+  return render_template('films.html', band=band, film=film)
 
-@app.route("/album/", methods=['POST', 'GET'])
-def album():
+@app.route("/actors/", methods=['POST', 'GET'])
+def actors():
   band = request.args.get('q', '')
   g.db = get_db()
   if band == '':
@@ -41,7 +31,7 @@ def album():
     cur.g.db.execute ('SELECT * FROM actor  where name = ?', (band, ))
   actor = [dict(name=row[0],
   age=row[1], known_for=row[2], nationality=row[3]) for row in cur.fetchall()]
-  return render_template('album.html', band=band, actor=actor)
+  return render_template('actors.html', band=band, actor=actor)
 
 @app.route("/search/", methods=['POST', 'GET'])
 def search():
